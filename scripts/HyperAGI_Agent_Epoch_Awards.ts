dbd3f9ddd578e347c9aaa4cc44905e6efb44c6f8
fangyeming <fangyemingzz@gmail.com>
2025-09-19 19:11:49 +0800

    Comment out admin role addition in HyperAGI_Agent_Epoch_Awards.ts

--- scripts/HyperAGI_Agent_Epoch_Awards.ts
+++ scripts/HyperAGI_Agent_Epoch_Awards.ts
@@ -11,11 +11,11 @@ async function main() {
 
   const HyperAGI_Roles_Cfg = await ethers.getContractAt('HyperAGI_Roles_Cfg', '0x7B33C8D43C52d0c575eACaEcFdAd68487bfB28Ea')
 
-  await (await HyperAGI_Roles_Cfg.addAdmin(instance.target)).wait()
+  // await (await HyperAGI_Roles_Cfg.addAdmin(instance.target)).wait()
 
   const HyperAGI_AgentWallet = await ethers.getContractAt('HyperAGI_AgentWallet', '0x6759Aa64749b8fE3E294E7A73Ce6ee14eBF4270d')
 
-  await (await HyperAGI_AgentWallet.grantRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', instance.target)).wait()
+  // await (await HyperAGI_AgentWallet.grantRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', instance.target)).wait()
 
   console.info('contractFactory address:', instance.target)
 
@@ -24,8 +24,6 @@ async function main() {
     address: implementationAddress,
     constructorArguments: [],
   })
-
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere q
